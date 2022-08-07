d1a911494e93b4e621b7781294cfe3f585c0592c
TNovoselska <tnovoselska@gmail.com>
2022-08-07 10:35:35 +0300

    Added search functions (JS)

---
+++ app.js
@@ -0,0 +1,68 @@
+let now = new Date();
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+  let currentHour = date.getHours();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+  let currentMinute = date.getMinutes();
+  if (currentMinute < 10) {
+    currentMinute = `0${currentMinute}`;
+  }
+  return `${currentDay} ${currentHour}:${currentMinute}`;
+}
+let date = document.querySelector("#date");
+date.innerHTML = formatDate(now);
+
+function showWeather(response) {
+  let currentTemp = Math.round(response.data.main.temp);
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = `${currentTemp}°`;
+  document.querySelector("#city").innerHTML = response.data.name;
+  let iconElement=document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  )
+  let descriptionElement=document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+function search(city) {
+  let apiKey = "bf6135db2ac746321352c2599aa8eaaf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+search(city);
+}
+let submit = document.querySelector("#submit");
+submit.addEventListener("click", handleSubmit);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+function showPosition (position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "bf6135db2ac746321352c2599aa8eaaf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getPosition (event){
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let currentBtn = document.querySelector("#current-btn");
+currentBtn.addEventListener("click", getPosition);
